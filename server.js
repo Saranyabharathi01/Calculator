230e349ca44b41217ad08396b8fb7460810bb75a
Saranyabharathi01 <saranyasaravanan2302@gmail.com>
2026-05-28 19:29:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Serve calculator.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "calculator.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Calculator app running at http://localhost:${PORT}`);
+});
